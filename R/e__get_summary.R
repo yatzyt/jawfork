00b4c67ce5011fdfafdf405c957c645123eec38e
elderneff <66916096+elderneff@users.noreply.github.com>
2023-07-17 16:10:20 -0600

    Trying to get all 5 quartiles
    This probably won't work

--- R/e__get_summary.R
+++ R/e__get_summary.R
@@ -27,8 +27,8 @@ e__get_summary <- function(session_name, current_row,outer_env=totem) {
 
   #y <- temp_df[my_filter, , drop = F]
   col <- temp_df[[current_row$column]]
-  Statistic <- c("Mean", "SD", "Median")
-  Value <- c(mean(col, na.rm = T), sd(col, na.rm = T), median(col, na.rm = T))
+  Statistic <- c("N", "Mean", "SD", "Median", "Q1", "Q3", "Min", "Max")
+  Value <- c(sum(!is.na(col)), mean(col, na.rm = T), sd(col, na.rm = T), quantile(col, prob = c(0.50, 0.25, 0.75, 0.00, 1.00), type = 1)
   y <- data.frame(Statistic, Value)
   
 
