13458a124245a0ef270fe63e13e5ba5ccf83b2e9
elderneff <66916096+elderneff@users.noreply.github.com>
2023-07-17 14:34:36 -0600

    Make Get Summary a clone of open flat view

--- R/e__all_event_functions.R
+++ R/e__all_event_functions.R
@@ -237,7 +237,7 @@ e__all_event_functions <- function(outer_env = totem) {
   }
   
   i__all_event_functions[["Full Data Table"]][["Get Summary"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
-    obj_env$df_obj$copy_full(obj_env$table_objects_list$current_row$column)
+    outer_env$u__flat_view(session_name, current_row)
   }
 
 
