de5cb569d94272e7cf018bc9fd0a16bef72c215c
elderneff <66916096+elderneff@users.noreply.github.com>
2023-07-20 13:17:59 -0600

    Update e__get_summary.R

--- R/e__get_summary.R
+++ R/e__get_summary.R
@@ -38,7 +38,7 @@ e__get_summary <- function(session_name, current_row,outer_env=totem) {
                                                                                                         Q1 =     quantile(eval(parse(text = current_row$column)), prob = c(0.25), type = 1, na.rm = T, names = F),
                                                                                                         Q3 =     quantile(eval(parse(text = current_row$column)), prob = c(0.75), type = 1, na.rm = T, names = F),
                                                                                                         Min =    quantile(eval(parse(text = current_row$column)), prob = c(0.00), type = 1, na.rm = T, names = F),
-                                                                                                        Max =    quantile(eval(parse(text = current_row$column)), prob = 1, type = 1, na.rm = T, names = F))                                                                                                        )
+                                                                                                        Max =    quantile(eval(parse(text = current_row$column)), prob = c(1.00), type = 1, na.rm = T, names = F))
     Output$MeanSD <- paste0(round(Output$Mean, digits = 4), " (", round(Output$SD, digits = 4), ")")
     Output$Q1Q3 <- paste0("(", Output$Q1, ", ", Output$Q3, ")")
     Output$MinMax <- paste0(Output$Min, ", ", Output$Max)
