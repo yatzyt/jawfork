4a2ff8552caa40beab12fcde0e7ce54e865aebf5
elderneff <66916096+elderneff@users.noreply.github.com>
2024-02-05 18:31:53 -0700

    Commenting graph_summary to try to fix crashing

--- R/e__all_event_functions.R
+++ R/e__all_event_functions.R
@@ -239,9 +239,9 @@ e__all_event_functions <- function(outer_env = totem) {
   i__all_event_functions[["Full Data Table"]][["Get Summary"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
     outer_env$u__get_summary(session_name, current_row)
   }
-  i__all_event_functions[["Full Data Table"]][["Graph Summary"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
-    outer_env$u__graph_summary(session_name, current_row)
-  }
+  #i__all_event_functions[["Full Data Table"]][["Graph Summary"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
+  #  outer_env$u__graph_summary(session_name, current_row)
+  #}
 
 
   #--------------------------------------------
