84ad7beb5d6e0ceca2ecd1bd5d20d62d48612116
elderneff <66916096+elderneff@users.noreply.github.com>
2024-02-08 14:39:25 -0700

    Adding labels and a title to no group plot

--- R/e__graph_summary.R
+++ R/e__graph_summary.R
@@ -51,7 +51,8 @@ e__graph_summary <- function(session_name, current_row,outer_env=totem) {
     if (class(temp_df[[current_row$column]]) %in% c("numeric", "integer")) {   
       #utils::writeClipboard(str = "Target column is numeric", format = 1) 
 
-      boxplot(temp_df[[current_row$column]])
+      #boxplot(temp_df[[current_row$column]])
+      eval(parse(text = sprintf('boxplot(temp_df[[current_row$column]], xlab = "", ylab = "%s", main = "%s")', "temp_df[[current_row$column]]", "temp_df[[current_row$column]]")))
     ### Otherwise no boxplot ###
     } else {    
       #utils::writeClipboard(str = "Target column is not numeric", format = 1)
