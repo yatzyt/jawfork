e4caf14f4557ffd2e8e0a2c0ee7b21eb14355196
elderneff <66916096+elderneff@users.noreply.github.com>
2024-02-08 17:05:04 -0700

    Everything works! vertical x axis labels looked awful though

--- R/e__graph_summary.R
+++ R/e__graph_summary.R
@@ -41,7 +41,7 @@ e__graph_summary <- function(session_name, current_row,outer_env=totem) {
       group_by_entry_asterisks <- gsub(", ", "*", group_by_entry)
       #Concatenate the independent and dependent variables into a string
       title <- sprintf('%s ~ %s', current_row$column, group_by_entry_asterisks)
-      eval(parse(text = sprintf('plot <- boxplot(%s ~ %s, temp_df, ylab = "%s", main = "%s", las = 2)', current_row$column, group_by_entry_asterisks, current_row$column, title)))
+      eval(parse(text = sprintf('plot <- boxplot(%s ~ %s, temp_df, ylab = "%s", main = "%s")', current_row$column, group_by_entry_asterisks, current_row$column, title)))
       text(1:length(plot$n), min(temp_df[[current_row$column]], na.rm = T) - ((max(temp_df[[current_row$column]], na.rm = T) - min(temp_df[[current_row$column]], na.rm = T)) * 0.02), paste("n=", plot$n))
       grid()
     ### Otherwise no boxplot ###
@@ -57,7 +57,7 @@ e__graph_summary <- function(session_name, current_row,outer_env=totem) {
       #utils::writeClipboard(str = "Target column is numeric", format = 1) 
 
       #boxplot(temp_df[[current_row$column]])
-      eval(parse(text = sprintf('plot <- boxplot(temp_df[[current_row$column]], xlab = "", ylab = "%s", main = "%s", las = 2)', current_row$column, current_row$column)))
+      eval(parse(text = sprintf('plot <- boxplot(temp_df[[current_row$column]], xlab = "", ylab = "%s", main = "%s")', current_row$column, current_row$column)))
       text(1:length(plot$n), min(temp_df[[current_row$column]], na.rm = T) - ((max(temp_df[[current_row$column]], na.rm = T) - min(temp_df[[current_row$column]], na.rm = T)) * 0.02), paste("n=", plot$n))
       grid()
     ### Otherwise no boxplot ###
