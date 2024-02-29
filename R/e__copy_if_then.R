09a4bf8eb9a02446a8110825f120d723489619da
elderneff <66916096+elderneff@users.noreply.github.com>
2024-02-28 22:36:26 -0700

    I don't know what I did, reverting to first commit plus typo fix

--- R/e__copy_if_then.R
+++ R/e__copy_if_then.R
@@ -25,7 +25,7 @@ e__copy_if_then <- function(session_name, current_row, df_obj,outer_env=totem) {
 
     j <- j + 1
   }
-  string_builder[j] <- paste0("else do;\n    err_msg=catx(\"|\",\"Err" || "or: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    put err_msg;\nend;")
+  string_builder[j] <- paste0("else do;\n    err_msg=catx(\"|\",\"Error: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    put err_msg;\nend;")
 
 
   utils::writeClipboard(str = charToRaw(paste0(paste0(string_builder, collapse = "\n"), " ")), format = 1)
@@ -53,12 +53,12 @@ e__copy_if_then_do <- function(session_name, current_row, df_obj,outer_env=totem
   string_builder[1] <- paste0("if      missing(", current_row$column, ") then do;\n    <var>=\"\";\nend;")
   j <- 2
   for (i in column_values) {
-    string_builder[j] <- paste0("if      missing(", current_row$column, ") then do;\n    <var>=\"\";\nend;")
+    string_builder[j] <- paste0("else if ", current_row$column, "=", sep, i, sep, " then do;\n    <var>=\"\";\nend;")
 
     j <- j + 1
   }
-  string_builder[j] <- paste0("else do;\n    err_msg=catx(\"|\",\"Err" || "or: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    put err_msg;\nend;")
+  string_builder[j] <- paste0("else do;\n    err_msg=catx(\"|\",\"Error: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    put err_msg;\nend;")
 
 
-  #utils::writeClipboard(str = charToRaw(paste0(paste0(string_builder, collapse = "\n"), " ")), format = 1)
+  utils::writeClipboard(str = charToRaw(paste0(paste0(string_builder, collapse = "\n"), " ")), format = 1)
 }
