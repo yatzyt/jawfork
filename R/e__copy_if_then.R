4f01d9595c33967dcee948a60a2747e1267f0500
elderneff <66916096+elderneff@users.noreply.github.com>
2024-02-28 22:29:26 -0700

    Fixing a typo in copy_if_then_do, testing dialog boxes

--- R/e__copy_if_then.R
+++ R/e__copy_if_then.R
@@ -25,7 +25,7 @@ e__copy_if_then <- function(session_name, current_row, df_obj,outer_env=totem) {
 
     j <- j + 1
   }
-  string_builder[j] <- paste0("else do;\n    err_msg=catx(\"|\",\"Error: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    put err_msg;\nend;")
+  string_builder[j] <- paste0("else do;\n    err_msg=catx(\"|\",\"Err" || "or: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    put err_msg;\nend;")
 
 
   utils::writeClipboard(str = charToRaw(paste0(paste0(string_builder, collapse = "\n"), " ")), format = 1)
@@ -49,16 +49,44 @@ e__copy_if_then_do <- function(session_name, current_row, df_obj,outer_env=totem
     sep <- "\""
   }
 
+    ######################## Dialog box test
+      dialog <- RGtk2::gtkMessageDialog(
+        parent = outer_env[[session_name]]$windows$main_window,
+        flags = "destroy-with-parent",
+        type = "question",
+        buttons = "upper-lower-cancel",
+        "Code in upper or lower case?"
+      )
+      #dialog["secondary-text"] <- cmd
+
+
+      ###################################################
+      ### code chunk number 62: Pre-defined-dialogs.Rnw:49-58
+      ###################################################
+      response <- RGtk2::gtkDialogRun(dialog)
+      if (response == RGtk2::GtkResponseType["cancel"] ||
+        response == RGtk2::GtkResponseType["close"] ||
+        response == RGtk2::GtkResponseType["delete-event"]) {
+        ## pass
+      } else if (response == RGtk2::GtkResponseType["upper"]) {
+        clipr::write_clip(str = 'UPPER', format = 1)
+      } else if (response == RGtk2::GtkResponseType["lower"]) {
+        clipr::write_clip(str = 'lower', format = 1)
+      }
+      RGtk2::gtkWidgetDestroy(dialog)
+    ######################## Dialog box test
+
+
   string_builder <- rep(NA, length(column_values) + 2)
-  string_builder[j] <- paste0("if      missing(", current_row$column, ") then do;\n    <var>=\"\";\nend;")
+  string_builder[1] <- paste0("if      missing(", current_row$column, ") then do;\n    <var>=\"\";\nend;")
   j <- 2
   for (i in column_values) {
-    string_builder[j] <- paste0("else if ", current_row$column, "=", sep, i, sep, " then <var>=\"\";")
+    string_builder[j] <- paste0("if      missing(", current_row$column, ") then do;\n    <var>=\"\";\nend;")
 
     j <- j + 1
   }
-  string_builder[j] <- paste0("else do;\n    err_msg=catx(\"|\",\"Error: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    put err_msg;\nend;")
+  string_builder[j] <- paste0("else do;\n    err_msg=catx(\"|\",\"Err" || "or: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    put err_msg;\nend;")
 
 
-  utils::writeClipboard(str = charToRaw(paste0(paste0(string_builder, collapse = "\n"), " ")), format = 1)
+  #utils::writeClipboard(str = charToRaw(paste0(paste0(string_builder, collapse = "\n"), " ")), format = 1)
 }
