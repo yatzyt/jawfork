c19da0a925093ef08ca4de98c62f0814b677fea9
elderneff <66916096+elderneff@users.noreply.github.com>
2024-02-29 19:05:33 -0700

    IF_THEN_DO WORKS!! No significant changes, checkpoint

--- R/e__copy_if_then.R
+++ R/e__copy_if_then.R
@@ -65,7 +65,7 @@ e__copy_if_then <- function(session_name, current_row, df_obj,outer_env=totem) {
   }
   string_builder[j] <- paste0("else do;\n    err_msg=catx(\"|\",\"Error: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    put err_msg;\nend;")
 
-
+  #Copy final string to clipboard
   utils::writeClipboard(str = charToRaw(paste0(paste0(string_builder, collapse = "\n"), " ")), format = 1)
 }
 
@@ -128,9 +128,9 @@ e__copy_if_then_do <- function(session_name, current_row, df_obj,outer_env=totem
   
     string_builder <- rep(NA, length(column_values) + 2)
     if (toupper(selection) == "UPPERCASE") {
-      string_builder[1] <- paste0("IF      MISSING(", current_row$column, ") THEN DO;\n    <var> = \"\";\nEND;")
+      string_builder[1] <- paste0("IF MISSING(", current_row$column, ") THEN DO;\n    <var> = \"\";\nEND;")
     } else {
-      string_builder[1] <- paste0("if      missing(", current_row$column, ") then do;\n    <var>=\"\";\nend;")
+      string_builder[1] <- paste0("if missing(", current_row$column, ") then do;\n    <var>=\"\";\nend;")
     }
     j <- 2
     for (i in column_values) {
@@ -146,9 +146,9 @@ e__copy_if_then_do <- function(session_name, current_row, df_obj,outer_env=totem
       string_builder[j] <- paste0("ELSE DO;\n    ERR_MSG = CATX(\"|\",\"ERROR: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    PUT ERR_MSG;\nEND;")
     } else {
       string_builder[j] <- paste0("else do;\n    err_msg=catx(\"|\",\"ERROR: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    put err_msg;\nend;")
-    }
-  
-  
+    }  
+
+    #Copy final string to clipboard
     utils::writeClipboard(str = charToRaw(paste0(paste0(string_builder, collapse = "\n"), " ")), format = 1)
   }
 }
