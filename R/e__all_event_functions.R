b856331f480b6af7bd4e89d1e90fb569cf82538f
elderneff <66916096+elderneff@users.noreply.github.com>
2024-04-05 23:32:40 -0600

    Troubleshooting

--- R/e__all_event_functions.R
+++ R/e__all_event_functions.R
@@ -177,7 +177,8 @@ e__all_event_functions <- function(outer_env = totem) {
 
   i__all_event_functions[["Meta Table"]][["Move column before"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
     current_data <- obj_env$df_obj$current_data()
-    #row_i <- current_row$row_i
+    head(current_data)
+    row_i <- current_row$row_i
     #outer_env$move_column(0, session_name, current_data[row_i, "variable",
     #  drop = T
     #])
