b4e4f9a6cdc4314cfa4bd3427fca743d64b245f4
elderneff <66916096+elderneff@users.noreply.github.com>
2024-04-05 23:15:56 -0600

    Implementing code run after column move and move after

--- R/e__move_column.R
+++ R/e__move_column.R
@@ -23,8 +23,10 @@ e__move_column <- function(placement, session_name, current_row, outer_env=totem
   } else {
     col_order <- colnames(temp_df[0, ])
   }
-
-  #Handle moving column before target
+  
+  ##############################
+  # Get user's "before" target #
+  ##############################
   if (placement == 0) {
     #Generate dialog to ask about placement
     dialog <- gtkMessageDialog(
@@ -53,26 +55,65 @@ e__move_column <- function(placement, session_name, current_row, outer_env=totem
     target <- col_order[gtkComboBoxGetActive(combo) + 1]
     #Destroy dialog box
     gtkWidgetDestroy(dialog)
+  }
+  #############################
+  # Get user's "after" target #
+  #############################
+  if (placement == 1) {
+    #Generate dialog to ask about placement
+    dialog <- gtkMessageDialog(
+      parent = outer_env[[session_name]]$windows$main_window, 
+      flags = "destroy-with-parent", 
+      type = "question", 
+      buttons = "ok-cancel", 
+      paste0("Select a column to move ", toupper(selection), " after"))
     
-    if (response %in% c(GtkResponseType["close"], GtkResponseType["delete-event"], GtkResponseType["cancel"]) == F) {
-      old_index <- which(col_order == selection)
-      new_index <- which(col_order == target) + placement
-      if (new_index > old_index) {
-        delete_index <- old_index
-      } else {
-        delete_index <- old_index + 1
-      }
-      col_order <- append(col_order, selection, after = new_index - 1)
-      col_order <- col_order[-delete_index]
-      #Turn col_order into a comma separated list
-      newst <- ""
-      for (i in 1:length(col_order)) {
-        newst <- paste0(newst, col_order[i])
-        if (i != length(col_order)) {
-          newst <- paste0(newst, ", ")
-        }
-      }
-      RGtk2::gtkEntrySetText(outer_env[[session_name]]$data_view_list$select_entry, newst)
+    #Add options
+    choices <- col_order
+    combo <- gtkComboBoxNewText()
+    combo$show()
+    for (choice in choices) {
+      combo$appendText(choice)
     }
+    combo$setActive(0)
+    
+    #Make a frame for the options
+    frame <- gtkFrame(paste0("Column to move ", toupper(selection), " after"))
+    frame$add(combo)
+    dialog[["vbox"]]$add(frame)
+    #Require response before interacting with table
+    response <- dialog$run()  
+    #Find selection
+    target <- col_order[gtkComboBoxGetActive(combo) + 1]
+    #Destroy dialog box
+    gtkWidgetDestroy(dialog)
+  }
+    
+  #####################
+  # Rearrange columns #
+  #####################
+  if (response %in% c(GtkResponseType["close"], GtkResponseType["delete-event"], GtkResponseType["cancel"]) == F) {
+    old_index <- which(col_order == selection)
+    new_index <- which(col_order == target) + placement
+    if (new_index > old_index) {
+      delete_index <- old_index
+    } else {
+      delete_index <- old_index + 1
+    }
+    col_order <- append(col_order, selection, after = new_index - 1)
+    col_order <- col_order[-delete_index]
+    #Turn col_order into a comma separated list
+    newst <- ""
+    for (i in 1:length(col_order)) {
+      newst <- paste0(newst, col_order[i])
+      if (i != length(col_order)) {
+        newst <- paste0(newst, ", ")
+      }
+    }
+    #Replace select field with new column order, run code
+    RGtk2::gtkEntrySetText(outer_env[[session_name]]$data_view_list$select_entry, newst)
+    outer_env$show_load_window()
+    outer_env$u__load_dataset_filter(session_name)
+    outer_env$hide_load_window()
   }
 }
