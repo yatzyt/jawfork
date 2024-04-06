ef1bb338f23fe82e734bb5a399980f4acafb0ae1
elderneff <66916096+elderneff@users.noreply.github.com>
2024-04-06 00:11:38 -0600

    Adding General Bob options

--- R/e__all_event_functions.R
+++ R/e__all_event_functions.R
@@ -13,60 +13,37 @@ e__all_event_functions <- function(outer_env = totem) {
 
   #-------------------------------------------
 
-
-
-
   i__all_event_functions[["General"]][["View"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
     obj_env$df_obj$view()
   }
-
   i__all_event_functions[["General"]][["Refresh"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
     obj_env$df_obj$update_filter()
   }
-
-
-
-
-
-
-
   i__all_event_functions[["General"]][["Add to filter"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
     column <- current_row$column
     value <- current_row$value
     obj_env$filter_obj$add(column, value)
   }
-
-
   i__all_event_functions[["General"]][["Add to grepl to filter"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
     column <- current_row$column
     value <- current_row$value
     obj_env$filter_obj$add_grepl(column, value)
   }
-
   i__all_event_functions[["General"]][["Clear filter"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
     obj_env$filter_obj$clean()
   }
-
-
   i__all_event_functions[["General"]][["Add to arrange"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
     column <- current_row$column
     obj_env$order_by_obj$add(column)
   }
-
-
   i__all_event_functions[["General"]][["Clear arrange"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
     obj_env$order_by_obj$clean()
   }
-
   i__all_event_functions[["General"]][["Trigger code"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
     outer_env$show_load_window()
     outer_env$u__load_dataset_filter(session_name)
     outer_env$hide_load_window()
   }
-
-
-
-
   i__all_event_functions[["General"]][["Open Context Menu"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
     if (is.null(view_objects$event) == F) {
       click_btn <- view_objects$event$button
@@ -75,6 +52,11 @@ e__all_event_functions <- function(outer_env = totem) {
         activate.time = RGtk2::gdkEventGetTime(view_objects$event)
       )
     }
+  }
+  i__all_event_functions[["General"]][["Bob"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
+    outer_env$show_load_window()
+    Sys.sleep(10)
+    outer_env$hide_load_window()
   }
 
 
