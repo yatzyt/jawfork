bf676c84bb5f973ba272bd163af9ab5462bddb7c
elderneff <66916096+elderneff@users.noreply.github.com>
2024-05-18 15:37:37 -0600

    Try to recognize backspace and delete in one signal, make it not error

--- R/u__text_area.R
+++ R/u__text_area.R
@@ -20,12 +20,11 @@ u__add_text_area <- function(label, shift_function, session) {
                 },data=list(session,shift_function))
 
       #Attempt to recognize whenever code area is edited
-      RGtk2::gSignalConnect(temp_list$View, "backspace",  
-                function() {
-                  print("Text in code area deleted")
-                  return(TRUE)
-                }
-                )
+      RGtk2::gSignalConnect(temp_list$View, c("backspace", "delete-from-cursor"),  
+                              function() {
+                                print("Text in code area deleted")
+                              }
+                            )
 
 
 
