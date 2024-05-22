076fef38deda28676f138941da6d92df860156a2
elderneff <66916096+elderneff@users.noreply.github.com>
2024-05-21 23:37:44 -0600

    Do not add to timeline stack for command keys

--- R/u__text_area.R
+++ R/u__text_area.R
@@ -24,12 +24,16 @@ u__add_text_area <- function(label, shift_function, session) {
                     buffer <- RGtk2::gtkTextViewGetBuffer(view)
                     end_iter <- RGtk2::gtkTextBufferGetEndIter(buffer)
                     start_iter <- RGtk2::gtkTextBufferGetStartIter(buffer)
-                    #str <- RGtk2::gtkTextBufferGetText(buffer,
-                    #  start_iter$iter, end_iter$iter,
-                    #  include.hidden.chars = TRUE
-                    #)
-                  str <- event[["keyval"]]
-                    print(paste0("Detected signal: ", str))
+                    str <- RGtk2::gtkTextBufferGetText(buffer,
+                      start_iter$iter, end_iter$iter,
+                      include.hidden.chars = TRUE
+                    )
+                    single_key <- event[["keyval"]]
+                    #Do not add to timeline stack for the following keys:
+                    #Left and right ctrl, shift, and alt keys; caps lock, arrow keys, home, end, and tab
+                    if (!(single_key %in% c("65507", "65505", "65513", "16777215", "65506", "65508", "65514", "65361", "65362", "65363", "65364", "65360", "65367", "65289"))) {
+                      print(paste0("Detected signal: ", str))
+                    }
                   
                     return(TRUE)
                 },data=list(session,shift_function))
