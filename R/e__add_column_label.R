4d3a298d7decdc76aa57862f612feb7dffa2c130
elderneff <66916096+elderneff@users.noreply.github.com>
2024-10-25 22:39:55 -0600

    Messing with padding

--- R/e__add_column_label.R
+++ R/e__add_column_label.R
@@ -47,13 +47,13 @@ e__add_column_label <- function(treeviewcolumn, label, j, var_class = NULL, tool
     my_row <- data3[j - 1, ]
     z <- RGtk2::gtkLabel(my_row[, "label"])    
     z$xalign <- 0
-    RGtk2::gtkBoxPackStart(hb, z, T, T, padding = 1)
+    RGtk2::gtkBoxPackStart(hb, z, T, T, padding = 3)
   } else {
     y <- RGtk2::gtkLabel("")
     x <- RGtk2::gtkLabel(paste0(label, " "))
     x$xalign <- 0
     RGtk2::gtkWidgetModifyFont(x, font)
-    RGtk2::gtkBoxPackStart(hb, x, T, F, padding = 3)
+    RGtk2::gtkBoxPackStart(hb, x, T, F, padding = 1)
   }
 
 
