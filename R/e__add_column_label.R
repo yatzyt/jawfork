8d6c788976269dee3ffccd5dfaf06b6d597dfcca
elderneff <66916096+elderneff@users.noreply.github.com>
2024-10-25 22:22:58 -0600

    Testing custom messages in column headeres

--- R/e__add_column_label.R
+++ R/e__add_column_label.R
@@ -39,7 +39,8 @@ e__add_column_label <- function(treeviewcolumn, label, j, var_class = NULL, tool
     x$xalign <- 0
     RGtk2::gtkWidgetModifyFont(x, font)
     RGtk2::gtkBoxPackStart(hb, x, T, T, padding = 1)
-    y <- RGtk2::gtkLabel(sec_label)
+    #y <- RGtk2::gtkLabel(sec_label)
+    y <- RGtk2::gtkLabel("Test")
     RGtk2::gtkBoxPackStart(hb, y, F, F, padding = 1)
   } else {
     y <- RGtk2::gtkLabel("")
