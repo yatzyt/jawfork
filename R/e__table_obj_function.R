d209cb0342e69af438e905063cd7381a8e52f4a0
elderneff <66916096+elderneff@users.noreply.github.com>
2024-11-02 12:32:15 -0600

    What if I just don't set the label at all?

--- R/e__table_obj_function.R
+++ R/e__table_obj_function.R
@@ -177,7 +177,7 @@ e__table_obj_function <- function(box, outer_env = totem,obj_env=inner_env) {
           )
 
           #RGtk2::gtkLabelSetText(obj_env$table_objects_list$allColumns[[j]]$evt$y, paste0("U: ", my_row[, "unique"]))
-          RGtk2::gtkLabelSetText(obj_env$table_objects_list$allColumns[[j]]$evt$y, "Test column labels 2")
+          #RGtk2::gtkLabelSetText(obj_env$table_objects_list$allColumns[[j]]$evt$y, "Test column labels 2")
           RGtk2::gtkWidgetSetTooltipText(obj_env$table_objects_list$allColumns[[j]]$evt$evb, my_tool_tip)
           if (my_row[, "class"] == "numeric") {
             RGtk2::gtkWidgetModifyBg(object = obj_env$table_objects_list$allColumns[[j]]$evt$evb, state = "normal", color = "#FFFFFF")
